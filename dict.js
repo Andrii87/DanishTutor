753d73e305041f28d4575990d989ec613040a719
Andrii87 <vlasov.andrii@gmail.com>
2020-05-30 23:29:33 +0200

    Initial Upload

---
+++ dict.js
@@ -0,0 +1,144 @@
+dict={
+    chapter1:{
+        page1:{
+            grim:'ugly',
+            'skynd dig':'hurry up',
+            fridag:'day off',
+            julepynt:'Christmas decorations',
+            "rette ud":"straighten out",
+		  	'Det er bare dejligt':"It's just nice",
+            krydderier:'spices',
+            "et sprog":'language',
+            "sådan her":"like this",
+            "snart":"soon",
+        },
+        page2:{
+            'det var flot':'it was great',
+            'dernede':'down there',
+            'skatten':'treasure',
+            "kiste":"chest",
+            "utroligt":"incredible",
+            "skattejagt":"treasure hunt",
+            "udenfor":"outside",
+            "indenfor":"inside",
+            "meget koldt":"very cold",
+
+
+        },
+        page3:{
+            'faret vild':'got lost',
+            'snavset':'dirty',
+            'ikke såslemt':'not so bad',
+            'rod':'mess',
+            'rodehoved':'messy person',
+            'skrald':'garbage',
+            'de er mine':'those are mine',
+            'ryd op!':'tidy up!',
+            'hente nogle':'get some',
+            'svampen':'sponge',
+            
+        
+        },
+        page4:{
+            'har smidt':'have thrown',
+            'haveslange':'garden hose',
+            'tænde for':'turn on',
+            'slukke for':'turn off',
+            'lys':'light',
+            'sluk for lyset': 'turn off the light',
+            'tænd for lyset': 'turn on the light',
+            'hold op':'stop',
+            'alle sammen':'everyone',
+            'er våde':'are wet',
+            "han er våd":"he is wet",
+            'tør':'dry',
+            'ren':'clean',
+            'slemme':'bad',
+        },
+
+        page5:{
+            'huske':'remember',
+            'halstørklæde':'scarf',
+            'hue':'hat',
+            'huer':'hats',
+            'jeg vandt':'I won',
+            'snyd': 'cheating',
+            'yndlings': 'favorite',
+            'det jeg også':'so am I',
+            'virker':'works',
+            'begge fødder':'both feet',
+            "plaske":"splash",
+            'hold fast':'hold on',
+            'hold nu fast':'wow',
+            'øve':'practice',
+
+        }, 
+         page6:{
+            "spark":"kick",
+            'spark med benene':'kick with legs',
+            'lade være med':'stop it',
+            'store børn':'big kids',
+            "rigtig godt":"really good",
+            'tabt':'lost',
+            'ned':'down',
+            'op':'up',
+            "lige":"straight",
+            'venstre':'left',
+            'højre':'right',
+            'desværre':'unfortunately',
+
+        }, 
+         page7:{
+            "kedelig":"boring",
+            'vigtig':'important',
+            'aflevere':'deliver',
+            'gave':'present',
+            "løbet":"the race",
+            'nej det gjorde du ikke':"no you didn't",
+            'du kan ikke':"you can't",
+            'fange mig':'catch me',
+            "op ad bakke":"uphill",
+            'stejl bakke':'steep hill',
+            'smukke':'beautiful',
+            'hårdt arbejde':'hard work',
+            'ned ad bakken':'downhill',
+            'træt':'tired',
+
+
+        }, 
+         page8:{
+            "":"",
+            '':'',
+            '':'',
+            '':'',
+            "":"",
+            '':'',
+            '':'',
+            '':'',
+            "":"",
+            '':'',
+            '':'',
+            '':'',
+
+        }
+
+    },
+    chapter2:{
+        page1:{
+            folk:'people',
+            skal:'should',
+            flere:'more',
+            tiden:'the time',
+            rejse:'travel',
+            døde:'dead',
+            om:'about'
+        },
+        page2:{
+            'det sker':'it happens',
+            'med':'with',
+            'en smule':'a bit'
+
+
+        },
+    }
+}
